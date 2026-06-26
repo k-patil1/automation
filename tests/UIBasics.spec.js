8085874cc22576ca33685438ec3b4140ef8a8609
Komal Dinde <komal.tech7@gmail.com>
2026-06-26 15:09:09 +0530

    Added login 2

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -10,5 +10,6 @@ test.only('first playwrite test', async ({browser})=>
    await page.locator('input#password').fill('learning');
     await page.locator('input#signInBtn').click();
     await page.locator('output#signInBtn').click();
+    //git checkout main
 
 })
