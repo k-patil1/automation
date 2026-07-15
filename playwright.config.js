f6b74141d5fd9f17c52737e22570b4ddce3ab577
Komal Dinde <komal.tech7@gmail.com>
2026-07-15 13:55:53 +0530

    Added dev,qa and stage environment settings 1

--- playwright.config.js
+++ playwright.config.js
@@ -16,7 +16,7 @@ const config = defineConfig({
   use: {
     baseURL: ENV.baseURL,        // <-- Add this
     browserName: 'chromium',
-    headless: false,
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
